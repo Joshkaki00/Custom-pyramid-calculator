4f4fad67f5d980fdeef1a5beaed9e2d3c3c74dc1
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2024-09-09 22:19:36 -0700

    Add files via upload

---
+++ custom-calc.py
@@ -0,0 +1,44 @@
+# Homework: Your job is to make a custom calculator. 
+# Your calculator should accept at least three values. 
+
+# For example height, width, length
+
+# It should print a prompt that makes it clear what 
+# is being calculated. 
+
+print('Hello. This calculator is a pyramid calculator. ')
+
+units = input('Enter the units you want to use. E.g. cm, m, ft, etc. : ')
+length = int(input('Please enter the length: '))
+#print(type(length))
+width = int(input('Please enter the width: '))
+#print(type(width))
+height = int(input('Please enter the height: '))
+#print(type(height))
+
+
+total = int(length * width * height) / 3
+print('Your pyramid is', total, 'cubic', units + '!')
+
+
+
+# For example: 
+# Enter height, width, and length to calculate the area of a cube
+# Height: 3
+# Width: 4
+# Length: 2
+
+# After accepting input the calculator should perform 
+# an accurate calculation and display the results in 
+# a clear and well formatted message. 
+
+# For example: A cube with a height of 3, width of 4, and length of 2 has an area of 24
+
+# You can accept string input that becomes part of the descirption. 
+# For example: Input units: inches
+
+# Be sure to convert your numeric values to numbers before performing math operations!
+
+
+
+
